28d915a23dab4bd4c0a64800377f95ca56c30058
Quinton Shababngu <quinton.shabangu@entelect.co.za>
2018-04-15 20:21:38 +0200

    separated out the question service from the http client and made the client static

---
+++ Sport.Mobile.Shared/Services/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sport.Mobile.Shared.Services
+{
+    public static class Client
+    {
+        
+        private static string BaseUrl = "https://iziphicaphicwano.firebaseio.com/";
+        public static string Questions = BaseUrl + "games/0/questions/";
+        private static HttpClient client;
+
+        private static HttpClient HttpClient => client ?? (client = new HttpClient(new HttpClientHandler(), true));
+
+        public static Task<HttpResponseMessage> GetQuestion(int id)
+        {
+            return HttpClient.GetAsync(BaseUrl + $"games/0/questions/{id}.json");
+        }
+    }
+}
--- Sport.Mobile.Shared/Services/QuestionsService.cs
+++ Sport.Mobile.Shared/Services/QuestionsService.cs
@@ -11,19 +11,14 @@ namespace Sport.Mobile.Shared.Services
 {
     public class QuestionsService
     {
-        private HttpClient _client;
-
         public async Task<List<Question>> GetQuestions()
         {
             var ids = CreateUniqueIds();
             var questions = new List<Question>();
-            
-            _client = new HttpClient(new HttpClientHandler(), true);
 
             foreach (var id in ids)
             {
-                var uri = new Uri($"https://iziphicaphicwano.firebaseio.com/games/0/questions/{id}.json");
-                var response = await _client.GetAsync(uri);
+                var response = await Client.GetQuestion(id);
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync();
@@ -39,7 +34,7 @@ namespace Sport.Mobile.Shared.Services
         {
             var rnd = new Random();
             var ids = new List<int>();
-            while (ids.Count < 11)
+            while (ids.Count < 10)
             {
                 var nextId = rnd.Next(0, 50);
                 if (ids.Any(id => id != nextId) || ids.Count == 0 || nextId != 7)
